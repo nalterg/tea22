259fb92faa2e78821878b9d18b0aaea9b318a618
nalterg <126792634+nalterg@users.noreply.github.com>
2024-10-18 15:34:22 +0200

    Beispiele erstellt

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -2,11 +2,33 @@
 #include <fmt/format.h>
 
 
-int foo;
+static int foo = 42;
+
+void doSomething(int value) {
+    value += 1;
+    fmt::print("doSomething, value {}\n", value);
+}
+
+void doSomethingPtr(int* ptr) {
+    *ptr += 1;
+    fmt::print("doSomething, value {}\n", *ptr);
+}
+
+void doSomethingStatic() {
+    static int value = 56;// wird nur beim ersten aufführen gestzt, danach wachst die variable
+    value += 1;
+    fmt::print("doSomething, value {}\n", value);
+}
+
+int* doReturnAPtr(){
+    static int value = 4711;
+    fmt::print("doReturnAPtr, value {}\n", value);
+    return &value; // Niemals ein Zeiger innerhalb einer Funktion auf einer lokalen Variabel returnen(static bleibt statisch--> kp)
+}
+
 
 auto main(int argc, char **argv) -> int
-{
-  
+{  
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
@@ -15,7 +37,17 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello, {}!\n", argv[0]);
     fmt::print("Value of foo {} address of foo {}\n", foo, fmt::ptr(&foo));
 
+    int bar = 42;
+    doSomethingPtr(&bar);
+    fmt::print("The value of bar is: {}\n", bar);    
 
+    int* returnedPtr = doReturnAPtr();
+    fmt::print("The value of returnedPtr is: {}\n", *returnedPtr);
+    doSomething(32);
+    fmt::print("The value of returnedPtr is: {}\n", *returnedPtr);
 
+    doSomethingStatic();
+    doSomethingStatic();
+    doSomethingStatic();
     return 0; /* exit gracefully*/
 }
